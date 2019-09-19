9926b741cd9d9cd31739c27dd5dac3d6505b0dde
John Daley <johndaley.me@gmail.com>
2019-09-19 13:23:53 -0400

    fix(thunderball): report and log errors in render

--- packages/thunderball/src/server/browser/routing/render/index.js
+++ packages/thunderball/src/server/browser/routing/render/index.js
@@ -177,9 +177,7 @@ const render = (page, name, createRoutes, injectors = []) => {
           },
         );
       })
-      .catch((err) => {
-        next(JSON.stringify(err));
-      });
+      .catch(next);
   };
 };
 
